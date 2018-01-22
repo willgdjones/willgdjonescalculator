7e2bf3c050e0b9f454b308ac786b2219e9d5e39e
William Jones <williamgdjones@gmail.com>
2018-01-22 14:03:04 +0000

    fix letsencrypt

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('.well-known/acme-challenge/uN9MCASIEgwfuLWpLVLswYtzOSEgSJvjMC8MGJSLkow')
+@app.route('/.well-known/acme-challenge/uN9MCASIEgwfuLWpLVLswYtzOSEgSJvjMC8MGJSLkow')
 def letsencrypt():
     return 'uN9MCASIEgwfuLWpLVLswYtzOSEgSJvjMC8MGJSLkow.H5iM88zVcyI6quHQsgAP59M0DguawF5twO6AZrVdz6w'
 
